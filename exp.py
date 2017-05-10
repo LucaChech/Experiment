f8c4c31429fa4ce2fcefd4b1b66182c23f0b2c28
Luca Chech <luca.chech.16@psychlangsci.ucl.ac.uk>
2017-05-10 17:42:28 +0100

    Fixed block design

--- exp.py
+++ exp.py
@@ -44,6 +44,106 @@ my_dict ={z[0]:list(z[1:]) for z in zip(image_names,cat1,cat2,cat3,cat4,cat5,ts,
 
 random.shuffle(image_names)
 
+# DEFINING BLOCKS
+low_present_normal = []
+low_present_critical = []
+low_absent_normal = []
+low_absent_critical = []
+high_present_normal = []
+high_present_critical = []
+high_absent_normal = []
+high_absent_critical = []
+
+list_dict = {}
+list_dict['low_present_normal'] = low_present_normal
+list_dict['low_present_critical'] = low_present_critical
+list_dict['low_absent_normal'] = low_absent_normal
+list_dict['low_absent_critical'] = low_absent_critical
+list_dict['high_present_normal'] = high_present_normal
+list_dict['high_present_critical'] = high_present_critical
+list_dict['high_absent_normal'] = high_absent_normal
+list_dict['high_absent_critical'] = high_absent_critical
+
+def block(load,present_absent, normal_critical,target_list_name):
+    target_list = list_dict[target_list_name]
+    for each in my_dict:
+        if load in my_dict[each] and present_absent in my_dict[each] and normal_critical in my_dict[each]:
+            target_list.append(each)
+
+block('low','Target Present','Normal','low_present_normal')  
+block('low','Target Absent','Normal','low_absent_normal')
+block('low','Target Present','Critical','low_present_critical')  
+block('low','Target Absent','Critical','low_absent_critical')
+block('high','Target Present','Normal','high_present_normal')
+block('high','Target Present','Critical','high_present_critical')
+block('high','Target Absent','Normal','high_absent_normal')
+block('high','Target Absent','Critical','high_absent_critical')
+#print len(low_present_normal), len(low_absent_normal), len(low_present_critical), len(low_absent_critical)
+#print len(high_present_normal), len(high_absent_normal), len(high_present_critical), len(high_absent_critical)
+#--------------------
+
+low_one = []
+low_two = []
+high_one = []
+high_two= []
+
+low_one = []
+low_one.append(np.random.choice(low_present_normal, 50, replace= False))
+low_one.append(np.random.choice(low_present_critical, 10, replace= False))
+low_one.append(np.random.choice(low_absent_normal, 50, replace= False))
+low_one.append(np.random.choice(low_absent_critical, 10, replace= False))
+for each in low_one:
+    each = each.tolist()
+low_one = [item for sublist in low_one for item in sublist]
+print 'low_one',len(low_one),type(low_one)
+
+def complementary_low(list_name):
+    for each in list_name:
+        if each not in low_one:
+            low_two.append(each)
+complementary_low(low_present_normal)
+print len(low_two)
+complementary_low(low_present_critical)
+print len(low_two)
+complementary_low(low_absent_normal)
+print len(low_two)
+complementary_low(low_absent_critical)
+print len(low_two)
+#print block_two
+print 'low_two: ', len(low_two), type(low_two)
+
+high_one = []
+high_one.append(np.random.choice(high_present_normal, 50, replace= False))
+high_one.append(np.random.choice(high_present_critical, 10, replace= False))
+high_one.append(np.random.choice(high_absent_normal, 50, replace= False))
+high_one.append(np.random.choice(high_absent_critical, 10, replace= False))
+for each in high_one:
+    each = each.tolist()
+high_one = [item for sublist in high_one for item in sublist]
+print 'high_one: ',len(high_one),type(high_one)
+
+def complementary_high(list_name):
+    for each in list_name:
+        if each not in high_one:
+            high_two.append(each)
+complementary_high(high_present_normal)
+print len(high_two)
+complementary_high(high_present_critical)
+print len(high_two)
+complementary_high(high_absent_normal)
+print len(high_two)
+complementary_high(high_absent_critical)
+print len(high_two)
+#print block_two
+print 'high_two: ', len(high_two), type(high_two)
+
+
+### RANDOMIZING IMAGE SEQUENCE FOR EACH BLOCK
+random.shuffle(low_one)
+random.shuffle(low_two)
+random.shuffle(high_one)
+random.shuffle(high_two)
+
 # Store info about the experiment session
 expName = 'participant info'  # from the Builder filename that created this script
 expInfo = {u'session': u'001', u'participant': u''}
@@ -129,27 +229,52 @@ end_of_exp = visual.TextStim(win,
 height=0.12)
 
 #SOUND
-f = sound.Sound('C:/Users/luca.chech.16/Desktop/final/tonesUpdated2/Luca/tones/Subject 666/PureTone_F1025_t100.wav', secs=0.15) 
+tone1 = sound.Sound('C:/Users/luca.chech.16/Desktop/final/tonesUpdated2/Luca/tones/Subject 666/Tone 1.wav', secs=0.05)
+tone2 = sound.Sound('C:/Users/luca.chech.16/Desktop/final/tonesUpdated2/Luca/tones/Subject 666/Tone 2.wav', secs=0.05)
+tone3 = sound.Sound('C:/Users/luca.chech.16/Desktop/final/tonesUpdated2/Luca/tones/Subject 666/Tone 3.wav', secs=0.05)
+tone4 = sound.Sound('C:/Users/luca.chech.16/Desktop/final/tonesUpdated2/Luca/tones/Subject 666/Tone 4.wav', secs=0.05)
 
+sound_onset = [0.050] * 4 + [0.100] * 4 + [0.150] * 4 + [0.200] * 4 + [0.250] * 4
+sound_Hz = ['tone1'] * 5 + ['tone2'] * 5 + ['tone3'] * 5 + ['tone4'] * 5
+random.shuffle(sound_onset)
+random.shuffle(sound_Hz)
+onset_counter = 0
+hz_counter = 0
 
-
-def handle_trial(play):
+stimulus = 0
+print sound_onset, sound_Hz
+def handle_trial(play, block_name):
      global i
+     global stimulus
+     global sound_onset
+     global sound_Hz
+     global onset_counter
+     global hz_counter
+     block = block_dict[block_name]
      cross.draw() 
      win.flip()
      core.wait(1) ###Fixation Cross
-     image = visual.ImageStim(win=win, image= mypath + image_names[stimulus])
+     image = visual.ImageStim(win=win, image= mypath + block[stimulus])
      image.draw()
      win.flip()
      if play:
-        core.wait(0.500)
-        f.play()
+        core.wait(sound_onset[onset_counter])
+        which_sound = sound_Hz[hz_counter]
+        if which_sound == 'tone1':
+            tone1.play()
+        elif which_sound == 'tone2':
+            tone2.play()
+        elif which_sound == 'tone3':
+            tone3.play()
+        elif which_sound == 'tone4':
+            tone4.play()
      stopwatch.reset()
      core.wait(1, hogCPUperiod=1)
      lista = event.getKeys(keyList=['space'], timeStamped=stopwatch)
-     print lista
-     my_dict[image_names[stimulus]].append(lista)
-     name=my_dict[image_names[stimulus]][-4]
+     #print lista
+     my_dict[block[stimulus]].append(lista)
+     name=my_dict[block[stimulus]][-4]
+     #print name
      if 'pottedplant' in name:
          search_text = visual.TextStim(win, 'Was there a plant ?',
      wrapWidth=2,
@@ -166,7 +291,7 @@ def handle_trial(play):
      win.flip()
      stopwatch.reset()
      keys = event.waitKeys(keyList=['q','p','space'],timeStamped=stopwatch)
-     print keys
+     #print keys
      rt_space = -999
      wait = True
      for k in keys:
@@ -181,11 +306,11 @@ def handle_trial(play):
         if ('p' in letters) or ('q' in letters):
             wait = False
         
-     my_dict[image_names[stimulus]].append(keys)
-     my_dict[image_names[stimulus]].append(rt_space)
+     my_dict[block[stimulus]].append(keys)
+     my_dict[block[stimulus]].append(rt_space)
 
      i = i + 1
-     my_dict[image_names[stimulus]].append(i)
+     my_dict[block[stimulus]].append(i)
      stopwatch.reset()
 
 
@@ -205,30 +330,46 @@ Instructions_screen_2.draw()
 win.flip()
 event.waitKeys()
 
-for stimulus in range(len(my_dict)):
-    if i == 120:
-        response_keys.draw()
-        win.flip()
-        event.waitKeys()
-    if i == 240 or i == 360:
-        response_keys.draw()
-        win.flip()
-        event.waitKeys()
-    stopwatch = core.Clock()
-    event.clearEvents()
-    if 'Target Present' in my_dict[image_names[stimulus]] and 'Critical' in my_dict[image_names[stimulus]]:
-       handle_trial(True)
-       
-    elif 'Target Present' in my_dict[image_names[stimulus]] and 'Normal' in my_dict[image_names[stimulus]]:
-       handle_trial(False)
-        
-    elif 'Target Absent' in my_dict[image_names[stimulus]] and 'Critical' in my_dict[image_names[stimulus]]:
-       handle_trial(True)
-       
-    elif 'Target Absent' in my_dict[image_names[stimulus]] and 'Normal' in my_dict[image_names[stimulus]]:
-       handle_trial(False)
-       
-       
+block_dict = {}
+block_dict['high_one'] = high_one
+block_dict['high_two'] = high_two
+block_dict['low_one'] = low_one
+block_dict['low_two'] = low_two
+def block_handler(block_name):
+    global stimulus
+    global onset_counter
+    global hz_counter
+    block = block_dict[block_name]
+    for stimulus in range(10):
+        if i==10 or i==20 or i==30:
+            response_keys.draw()
+            win.flip()
+            event.waitKeys()
+        stopwatch = core.Clock()
+        event.clearEvents()
+        if 'Target Present' in my_dict[block[stimulus]] and 'Critical' in my_dict[block[stimulus]]:
+           handle_trial(True, block_name)
+           onset_counter = onset_counter + 1
+           hz_counter = hz_counter + 1
+           print sound_onset[onset_counter], sound_Hz[hz_counter]
+        elif 'Target Present' in my_dict[block[stimulus]] and 'Normal' in my_dict[block[stimulus]]:
+           handle_trial(False, block_name)
+        elif 'Target Absent' in my_dict[block[stimulus]] and 'Critical' in my_dict[block[stimulus]]:
+           handle_trial(True, block_name)
+           onset_counter = onset_counter + 1
+           hz_counter = hz_counter + 1
+           print sound_onset[onset_counter], sound_Hz[hz_counter]
+        elif 'Target Absent' in my_dict[block[stimulus]] and 'Normal' in my_dict[block[stimulus]]:
+           handle_trial(False, block_name)
+        print my_dict[block[stimulus]]
+block_handler('low_one')
+block_handler('high_one')
+block_handler('high_two')
+block_handler('low_two')
+
+
+
+
 end_of_exp.draw()
 win.flip()
 event.waitKeys()
